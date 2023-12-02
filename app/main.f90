dea2495f63e8c6f02a9a3daaf96efd830098bc10
Zdenek Grof <grofz@vscht.cz>
2023-12-02 07:22:34 +0100

    Day 02

--- app/main.f90
+++ app/main.f90
@@ -1,6 +1,9 @@
 program aoc2023
   use day2301_mod, only: day2301
+  use day2302_mod, only: day2302
   implicit none
 
+ !goto 02
   01 call day2301('inp/01/input.txt')
+  02 call day2302('inp/02/input.txt')
 end program aoc2023
